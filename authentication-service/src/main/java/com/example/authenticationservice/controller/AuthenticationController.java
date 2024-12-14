c292637c98ee62e6ddd2524a8c19cd431bba899c
Daria <daria@Darias-MacBook-Pro.local>
2024-12-14 17:58:37 +0200

    Modified AuthenticationController RequestMapping

--- authentication-service/src/main/java/com/example/authenticationservice/controller/AuthenticationController.java
+++ authentication-service/src/main/java/com/example/authenticationservice/controller/AuthenticationController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 @AllArgsConstructor
 public class AuthenticationController {
     private AuthenticationService authenticationService;
